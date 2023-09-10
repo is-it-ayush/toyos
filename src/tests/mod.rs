90f3f76e957226c0fd47ad17726fe2d94e2fba8a
Ayush Gupta <ayush.keshav2004@gmail.com>
2023-09-11 00:02:06 +0530

    chore: write a simple test to test the test runner (hehe, a tounge twister)

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,6 +1,8 @@
 //! A custom test runner. We can't use the rust's test runnner
 //! since it relies upon std lib.
 
+use super::{println, print};
+
 #[cfg(test)]
 pub fn test_runner(tests: &[&dyn Fn()]) {
     use crate::println;
@@ -9,4 +11,12 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+}
+
+
+#[test_case]
+pub fn test_test() {
+    print!("Testing the runner...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
